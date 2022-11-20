b832795161f9a56bcc00e4e1ee7474ec1588d2b9
sarahnovaes <118676911+sarahnovaes@users.noreply.github.com>
2022-11-20 12:24:45 -0300

    Add files via upload

---
+++ main6.js
@@ -0,0 +1,27 @@
+var nome = ['Fabiana Araújo', 'Gabriel Gomes', 'Fernando Henrique', 'Ana Luiza', 'Geralda do Nascimento', 'Miguel Souza', 'Antonio Miguel'];
+
+var idade = ['33', '25', '17', '2', '93', '70', '69'];
+
+var i = 0
+
+while (i <= idade.length){
+  if (idade [i] >= 18){
+    console.log(nome[i], idade[i]);
+  } else{
+
+  }
+}
+
+
+//function ArrayAvg(idade) {
+ // var i = 0, summ = 0, ArrayLen = idade.length;
+ // while (i < ArrayLen) {
+  //    summ = summ + idade[i++];
+//}
+ // return summ / ArrayLen;
+//}
+//var idade = [33, 25, 17, 2, 93, 70, 69];
+//var a = ArrayAvg(idade);
+//console.log(a)
+
+//console.log(id[0],nome[0], idade[0]);
